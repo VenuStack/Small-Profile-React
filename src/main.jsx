d11da1221728a7bc05892e32ae1774471ee49c1d
VenuStack <venurrv143@gmail.com>
2026-08-18 12:42:11 +0530

    Fix profile card

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,36 @@
-import {createRoot} from "react-dom/client"
-
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import image from './assets/venuk.jpeg'
-function ProfileCard()
-{
-  return(
+import image from "./assets/venuk.jpeg";
+
+function ProfileCard() {
+  return (
     <div id="main1">
-    <div id="card">
-      <img id="img" src={image} alt="" />
-      <h1>VenuKumar</h1>
-      <h6>Web Developer</h6>
-      <p>I am a Computer Science Engineering student and an aspiring Full Stack Developer. 
-        I am passionate about building web applications and currently learning 
-        Java, React, SQL, and Spring Boot. I enjoy learning new technologies and 
-        improving my programming skills.</p>
-          <div className="link">
-          <a href="http://linkedin.com">linkedin</a>
-          <a href="google.com">google</a>
-          <a href="twitter.com">twitter</a>
-          </div>
+      <div id="card">
+        <img id="img" src={image} alt="" />
+
+        <h1>VenuKumar</h1>
+
+        <h6>Web Developer</h6>
+
+        <p>
+          I am a Computer Science Engineering student and an aspiring Full
+          Stack Developer. I am passionate about building web applications
+          and currently learning Java, React, SQL, and Spring Boot. I enjoy
+          learning new technologies and improving my programming skills.
+        </p>
+
+        <div className="link">
+          <a href="https://www.linkedin.com/in/kummari-venu-4b409331b/">LinkedIn</a>
+          <a href="https://github.com/VenuStack">GitHub</a>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-const root=createRoot(document.getElementById("root")).
+const root = createRoot(document.getElementById("root"));
 
-render(
-
+root.render(
   <ProfileCard />
-
-
-)
+);
